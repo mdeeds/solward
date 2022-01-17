c260f7c481ef18ad979dbac44f15e33e50c5b14f
Matt Deeds <mdeeds@gmail.com>
2022-01-17 10:18:33 -0800

    Add asynchronous ending of mission

--- ts/mission.ts
+++ ts/mission.ts
@@ -1,14 +1,21 @@
 import * as THREE from "three";
+import { ConeBufferGeometry } from "three";
+
+export type MissionStatus = 'complete' | 'failed' | 'aborted';
+export class MissionResult {
+  constructor(readonly status: MissionStatus) { }
+}
+type ResultCallback = (result: MissionResult) => void;
 
 export interface Mission {
   getInitialPlayerPosition(): THREE.Vector3;
   getInitialRotation(): THREE.Euler;
   tick(elapsedS: number, deltaS: number): void;
-  isComplete(): boolean;
-  isFailed(): boolean;
+  result(): Promise<MissionResult>;
 }
 
 export class Mission1 implements Mission {
+  private callbacks: ResultCallback[] = [];
   constructor(private system: THREE.Group | THREE.Scene) { }
   getInitialPlayerPosition(): THREE.Vector3 {
     // // A-230
@@ -21,19 +28,23 @@ export class Mission1 implements Mission {
   }
 
   tick(elapsedS: number, deltaS: number) {
-
+    this.tmp.copy(this.targetPosition);
+    this.tmp.add(this.system.position);
+    const remainingDistance = this.tmp.length();
+    if (remainingDistance < 10) {
+      const result = new MissionResult('complete');
+      for (const cb of this.callbacks) {
+        cb(result);
+      }
+    }
   }
 
   private targetPosition = new THREE.Vector3(233, -55.8, 223.6);
   private tmp = new THREE.Vector3();
-  isComplete() {
-    this.tmp.copy(this.targetPosition);
-    this.tmp.add(this.system.position);
-    const remainingDistance = this.tmp.length();
-    return remainingDistance < 10;
-  }
 
-  isFailed() {
-    return true;
+  result(): Promise<MissionResult> {
+    return new Promise<MissionResult>((resolve) => {
+      this.callbacks.push(resolve);
+    });
   }
 }
